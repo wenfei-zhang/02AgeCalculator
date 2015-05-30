5c5274f040e66785c78a99c8caa9af78bc25efb8
amy zhang <amyzhang@wenfeis-MacBook-Air.local>
2015-05-30 16:17:21 -0700

    Initial Commit

---
+++ 02AgeCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  02AgeCalculator
+//
+//  Created by amy zhang on 5/30/15.
+//  Copyright (c) 2015 amy zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
